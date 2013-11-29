6ca663c86008d0a070495eb810f047f311eb6779
Rhys Stansfield <rhys@onlinesalesgrowth.com>
2013-11-29 09:47:45 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yalp::Application.config.secret_key_base = '7c74ce704d76cc54f53fe84c45e9cfe0afe20653dda919027b6cd73e37cf600ec674d03e9c4f1a54533f4502e91857643fe9a02e98f7a0df2f23c57894a54014'
